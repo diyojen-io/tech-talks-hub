69b842263af6b72b56e44391cf38c1729485826b
alperen <alperen61@gmail.com>
2024-10-14 11:15:59 +0300

    Add login and signup modal with Formik validation and Yup schema for authentication.

--- src/app/modals/LoginModal/LoginModal.tsx
+++ src/app/modals/LoginModal/LoginModal.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useState } from "react";
+import {Formik, Form,Field,ErrorMessage} from "formik"; 
+import * as Yup from "yup";
 import BaseModal from "../BaseModal/BaseModal";
 import BaseButton from "@/app/components/BaseButton/BaseButton";
 import "./LoginModal.scss";
@@ -14,47 +15,61 @@ interface LoginModalProps {
 const LoginModal: React.FC<LoginModalProps> = ({ isOpen, onClose }) => {
   const { login } = useAuth();
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    login(username, password);
+  const validationSchema = Yup.object().shape({
+    username: Yup.string().required("Username is required"),
+    password: Yup.string().required("Password is required"),
+  });
+  
+  const handleSubmit = (value:{username:string, password:string})=>{
+    login(value.username, value.password);
     onClose();
-  };
+  }
 
   return (
     <BaseModal isOpen={isOpen} onClose={onClose}>
       <h2 className="login-modal__title">Login</h2>
-      <form className="login-modal__form" onSubmit={handleSubmit}>
-        <div className="login-modal__form-group">
-          <label className="login-modal__form-group__label" htmlFor="username">
-            Username
-          </label>
-          <input
-            className="login-modal__form-group__input"
-            type="text"
-            id="username"
-            name="username"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div className="login-modal__form-group">
-          <label className="login-modal__form-group__label" htmlFor="password">
-            Password
-          </label>
-          <input
-            className="login-modal__form-group__input"
-            type="password"
-            id="password"
-            name="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <BaseButton type="submit" label="Login" size="large" />
-      </form>
+     <Formik
+      initialValues={{ username: "", password: "" }}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+      >
+        {({})=>(
+          <Form>
+            <div className="login-modal__form-group">
+              <label className="login-modal__form-group__label" htmlFor="username">
+                Username
+              </label>
+              <Field
+                className="login-modal__form-group__input"
+                type="text"
+                id="username"
+                name="username"
+              />
+              <ErrorMessage name="username" component="div" className="error-message" />
+            </div>
+            <div className="login-modal__form-group">
+              <label className="login-modal__form-group__label" htmlFor="password">
+                Password
+              </label>
+              <Field
+                className="login-modal__form-group__input"
+                type="password"
+                id="password"
+                name="password"
+              />
+              <ErrorMessage name="password" component="div" className="login-modal__error" />
+            </div>
+            <BaseButton type="submit" label="Login" size="large" 
+             />
+          </Form>   
+          )}
+      </Formik>
     </BaseModal>
   );
 };
 
 export default LoginModal;
+
+
+        
+   
--- src/app/modals/SignUpModal/SignUpModal.tsx
+++ src/app/modals/SignUpModal/SignUpModal.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import BaseModal from "../BaseModal/BaseModal";
 import BaseButton from "@/app/components/BaseButton/BaseButton";
 import "./SignUpModal.scss";
@@ -14,64 +15,73 @@ interface SignUpModalProps {
 const SignUpModal: React.FC<SignUpModalProps> = ({ isOpen, onClose }) => {
   const { register } = useAuth();
 
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const validationSchema = Yup.object().shape({
+    username: Yup.string()
+      .min(5, "Username must be at least 5 characters")
+      .required("Username is required"),
+    email: Yup.string()
+      .email("Invalid email format")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(6, "Password must be at least 6 characters")
+      .required("Password is required"),
+  });
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    register(username, email, password);
+  const handleSubmit = (values: { username: string; email: string; password: string }) => {
+    register(values.username, values.email, values.password);
     onClose();
   };
-
+ 
   return (
     <BaseModal isOpen={isOpen} onClose={onClose}>
       <h2 className="sign-up-modal__title">Sign Up</h2>
-      <form className="sign-up-modal__form" onSubmit={handleSubmit}>
-        <div className="sign-up-modal__form-group">
-          <label
-            className="sign-up-modal__form-group__label"
-            htmlFor="username"
-          >
-            Username:
-          </label>
-          <input
-            className="sign-up-modal__form-group__input"
-            type="text"
-            id="username"
-            name="username"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div className="sign-up-modal__form-group">
-          <label className="sign-up-modal__form-group__label" htmlFor="email">
-            Email:
-          </label>
-          <input
-            className="sign-up-modal__form-group__input"
-            type="email"
-            id="email"
-            name="email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="sign-up-modal__form-group">
-          <label
-            className="sign-up-modal__form-group__label"
-            htmlFor="password"
-          >
-            Password:
-          </label>
-          <input
-            className="sign-up-modal__form-group__input"
-            type="password"
-            id="password"
-            name="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <BaseButton type="submit" label="Sign up" size="large" />
-      </form>
+      <Formik
+        initialValues={{ username: "", email: "", password: "" }}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {() => (
+          <Form className="sign-up-modal__form">
+            <div className="sign-up-modal__form-group">
+              <label className="sign-up-modal__form-group__label" htmlFor="username">
+                Username
+              </label>
+              <Field
+                className="sign-up-modal__form-group__input"
+                type="text"
+                id="username"
+                name="username"
+              />
+              <ErrorMessage component="div" className="sign-up-modal__error" name="username" />
+            </div>
+            <div className="sign-up-modal__form-group">
+              <label className="sign-up-modal__form-group__label" htmlFor="email">
+                Email
+              </label>
+              <Field
+                className="sign-up-modal__form-group__input"
+                type="email"
+                id="email"
+                name="email"
+              />
+              <ErrorMessage component="div" className="sign-up-modal__error" name="email" />
+            </div>
+            <div className="sign-up-modal__form-group">
+              <label className="sign-up-modal__form-group__label" htmlFor="password">
+                Password
+              </label>
+              <Field
+                className="sign-up-modal__form-group__input"
+                type="password"
+                id="password"
+                name="password"
+              />
+              <ErrorMessage component="div" className="sign-up-modal__error" name="password" />
+            </div>
+            <BaseButton type="submit" label="Sign up" size="large"/>
+          </Form>
+        )}
+      </Formik>
     </BaseModal>
   );
 };
